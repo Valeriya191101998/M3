d97e21f8e7a10cca502929aa5d6d0d52a833f271
Valeriya19101998 <petrova_valeriya_1998@mail.ru>
2020-12-24 22:35:36 +0300

    added cat5.java

---
+++ Cat5/src/Cat.java
@@ -0,0 +1,69 @@
+
+public class Cat
+{
+    private double originWeight;
+    private double weight;
+
+    private double minWeight;
+    private double maxWeight;
+
+
+    public Cat()
+    {
+        weight = 1500.0 + 3000.0 * Math.random();
+        originWeight = weight;
+        minWeight = 1000.0;
+        maxWeight = 9000.0;
+
+    }
+    public Cat (double weight)
+    {
+
+        this.weight = weight;
+
+    }
+
+    static Cat getKitten(double weight)
+    {
+        return new Cat(weight);
+    }
+
+
+    public void meow()
+    {
+        weight = weight - 1;
+        System.out.println("Meow");
+    }
+
+    public void feed(Double amount)
+    {
+        weight = weight + amount;
+    }
+
+    public void drink(Double amount)
+    {
+        weight = weight + amount;
+    }
+
+    public double getWeight()
+    {
+
+        return weight;
+    }
+
+    public String getStatus()
+    {
+        if(weight < minWeight) {
+            return "Dead";
+        }
+        else if(weight > maxWeight) {
+            return "Exploded";
+        }
+        else if(weight > originWeight) {
+            return "Sleeping";
+        }
+        else {
+            return "Playing";
+        }
+    }
+}
